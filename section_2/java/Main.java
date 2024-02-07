42bbe852b7c8309e0d5ad8b951add7ce02f72b51
Kyle Dewey <kyle.dewey@csun.edu>
2024-02-07 09:09:37 -0800

    code from class

--- section_2/java/Main.java
+++ section_2/java/Main.java
@@ -23,17 +23,34 @@ public class Main {
         return retval;
     }
 
-    // FOR MONDAY: subtyping
+    // Compile time: when the program is compiled
+    // javac command
+    //
+    // Runtime: when the program runs
+    // java command
+    
+    // 1.) ad-hoc polymorphism / virtual dispatch / dynamic dispatch
+    //     Method actually called is determined at runtime.
+    //     Runtime thing - java command.
+    //
+    // 2.) subtyping polymorphism
+    //     Use an object of a more specific type than what was requested
+    //     Compile-time thing - javac command
     public static void main(String[] args) {
         String filename = getFileName(args);
         IPAddress address = getNetworkLocation(args);
         Destination destination = null;
         if (filename != null) {
             // subtyping polymorphism
+            // Destination = FileDestination
             destination = new FileDestination(new FileWriter(filename));
         } else if (address != null) {
+            // subtyping polymorphism
+            // Destination = NetworkDestination
             destination = new NetworkDestination(new NetworkSocket(address));
         } else {
+            // subtyping polymorphism
+            // Destination = TerminalDestination
             destination = new TerminalDestination();
         }
         int result = doComputation(destination);
