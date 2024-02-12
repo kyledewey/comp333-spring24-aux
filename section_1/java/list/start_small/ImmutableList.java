7161bc7326b3e76d0cd1cc4836a3eb500d298a5c
Kyle Dewey <kyle.dewey@csun.edu>
2024-02-12 13:57:18 -0800

    code from class

--- section_1/java/list/start_small/ImmutableList.java
+++ section_1/java/list/start_small/ImmutableList.java
@@ -1,4 +1,5 @@
 // [1]: new Cons(1, new Nil())
+// ((Cons)((Cons)l0).tail).tail
 // [1, 2]: // ImmutableList l0 = new Cons(1, new Cons(2, new Nil()));
 //         ImmutableList l0 = new Nil();
 //         ((Cons)((Cons)l0).tail).tail
